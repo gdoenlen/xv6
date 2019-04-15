0de5d10e9be14fa0c32639ae2a306c60adc7ec97
George Doenlen <gdoenlen@gmail.com>
2019-04-15 14:44:02 -0400

    fix wait to only work with fork processes and not clones

--- proc.c
+++ proc.c
@@ -281,7 +281,7 @@ wait(void)
     // Scan through table looking for exited children.
     havekids = 0;
     for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-      if(p->parent != curproc)
+      if(p->parent != curproc || p->tstack != 0)
         continue;
       havekids = 1;
       if(p->state == ZOMBIE){
